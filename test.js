5603a04d72ce61fdc786aadb477f8d0c9666f715
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-11-21 12:01:43 +0100

    [fix]

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ const { task, status, data, execution, InvalidMessage } = require('.');
 
 /* Create Task Message */
 try {
-    const msgOne = task.createMessage(task.MESSAGE_TYPES.PEPE);
-    console.log(msgOne);
+    const m1 = task.createMessage(task.MESSAGE_TYPES.TASK_CREATE, {});
+    console.log(m1.id);
+    task.createMessage(task.MESSAGE_TYPES.TEST);
 } catch (err) {
     if (err instanceof InvalidMessage) {
         console.log('Invalid Message');
